07584a377a47e2da4ba93e0436c39ca5a1e292d9
hyojeong-commits <kweunsungo@gmail.com>
2026-03-13 11:53:08 +0900

    Update 1-2.c

--- code/1-2.c
+++ code/1-2.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 int main()
 {
-
+     printf("% 5s %s %s\n", "name", "id", "value");
+     printf("% 5s %d %f\n", "kim", 12, 4.12); 
+     printf("% 5s %d %f\n", "lee", 1922, 3.21);
+     printf("% 5s %d %f\n", park", 432, 1.0);
 
     return 0;
 }
