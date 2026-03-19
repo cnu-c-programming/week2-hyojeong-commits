1162156e1abc318cab5ead48a5db3f9fae29da9d
hyojeong-commits <kweunsungo@gmail.com>
2026-03-19 21:49:10 +0900

    Update 4-3.c

--- code/4-3.c
+++ code/4-3.c
@@ -2,7 +2,21 @@
 
 int main()
 {
+int n;
+    scanf("%d", &n);
 
+    if (n < 2) {
+        printf("false\n");
+        return 0;
+    }
+
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            printf("false\n");
+            return 0;
+        }
+    }
+    printf("true\n");
     return 0;
 }
 
