7cd490365d5f3d6b809ce2624ed86a65d80079ed
hyojeong-commits <kweunsungo@gmail.com>
2026-03-19 21:47:48 +0900

    Update 3-4.c

--- code/3-4.c
+++ code/3-4.c
@@ -2,7 +2,17 @@
 
 int main()
 {
+int a, b, c;
 
+    a = b = c = 5;
+    printf("%d %d %d\n", a, b, c);  // 5 5 5
+
+    a = 2 + 3 * 4;
+    printf("%d %d %d\n", a, b, c);  // 14 5 5
+
+    c = a++ + ++b;
+    printf("%d %d %d\n", a, b, c);  // 15 6 20
+    return 0;
     return 0;
 }
 
