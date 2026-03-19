b7233516c8523e696dcadb8146013dc3abf4bb74
hyojeong-commits <kweunsungo@gmail.com>
2026-03-19 21:41:51 +0900

    Update 2-3.c

--- code/2-3.c
+++ code/2-3.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
 int main()
-{
+{unsigned int x = 0x12345678;
+    unsigned char* addr = (unsigned char*)&x;
+    printf("%x\n", *addr);
+    printf("%x\n", *(addr + 1));
+    printf("%x\n", *(addr + 2));
+    printf("%x\n", *(addr + 3));
 
     return 0;
 }
